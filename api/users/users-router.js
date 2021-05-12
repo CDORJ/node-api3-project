30bac7f0f51dfdf8835c9fe6152e8f849b58ebb5
ORiveraJr84 <oriverajr84@gmail.com>
2021-05-11 21:38:30 -0400

    Removed posts-router

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -42,15 +42,58 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+  try {
+    const confirmation = await Users.update(id, changes);
+
+    if (confirmation) {
+      const updatedUser = await Users.getById(id);
+      res.status(200).json(updatedUser);
+    } else {
+      res.status(404).json({ message: "Invalid ID" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Sorry. Something went wrong." });
+  }
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
   // RETURN THE FRESHLY DELETED USER OBJECT
   // this needs a middleware to verify user id
+  try {
+    const deletedUser = await Users.getById(id);
+    const confirmation = await Users.remove(id);
+    if (confirmation) {
+      res.status(200).json(deletedUser);
+    } else {
+      res.status(404).json({ message: "Invalid ID" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Sorry. Something went wrong." });
+  }
+});
+
+router.get("/:id/posts", async (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  // RETURN THE ARRAY OF USER POSTS
+  // this needs a middleware to verify user id
+  try {
+    const posts = await Posts.getById(id);
+    res.status(200).json(posts);
+  } catch (error) {}
+});
+
+router.post("/:id/posts", (req, res) => {
+  // RETURN THE NEWLY CREATED USER POST
+  // this needs a middleware to verify user id
+  // and another middleware to check that the request body is valid
 });
 
 // do not forget to export the router
