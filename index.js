b3bf23cca8efc5b2c07b7fb3c3a2ae33610933b4
ORiveraJr84 <oriverajr84@gmail.com>
2021-05-11 20:48:52 -0400

    Server up and running.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // require your server and launch it
+const server = require("./api/server");
+
+const PORT = 4000;
+
+server.listen(PORT, () => {
+  console.log(`*** Server running on port $3000 ***`);
+});
